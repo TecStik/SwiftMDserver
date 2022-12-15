fd205a91f276027fdc1e6b78755ab51aef47f999
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2022-12-15 15:18:28 +0500

    fixed error message

--- server.js
+++ server.js
@@ -867,7 +867,7 @@ console.log("doc in summary",doc);
                         })
                     });
 
-                } else { res.send("error in finding clinic"+error) }
+                } else { res.send("error in finding clinic"+err) }
             })
             console.log(TotalPatient, TotalAmount, "addAmount");
         })
