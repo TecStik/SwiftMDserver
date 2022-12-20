1b630197bb9e50fdaa6560db58dcf1ba343c7fbf
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2022-12-20 10:43:30 +0500

    updated the db link

--- core/index.js
+++ core/index.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 5000;
-const dbURI = process.env.dbURI || "mongodb+srv://medical:2468@cluster0.z0q9h.mongodb.net/?retryWrites=true&w=majority";
+const dbURI = process.env.dbURI || "mongodb+srv://tecstik:Pakvip123@swiftmdcluster0.r7z21tp.mongodb.net/?retryWrites=true&w=majority";//"mongodb+srv://medical:2468@cluster0.z0q9h.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
     PORT: PORT,
